f784b0c2fbcc112bb694f9b56f2f0f1fab8969df
elnurxo <elnurykh@code.edu.az>
2022-09-20 09:36:12 +0400

    init debounce task

---
+++ app.js
@@ -0,0 +1,18 @@
+function debounce(callback, delay, immediate = false) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+    const shouldCallImmediately = timerId == null && immediate;
+    if (shouldCallImmediately) callback.apply(this, args);
+
+    timerId = setTimeout(() => {
+      if (!immediate) {
+        callback.apply(this, args);
+      }
+      timerId = null;
+    }, delay);
+  };
+}
+
+exports.debounce = debounce;
